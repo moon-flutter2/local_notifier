41b7b31cb6898c2954a1e9845cb4ed1d4035d8d1
moon-flutter2 <flutter2@moontechnolabs.com>
2024-11-20 18:00:25 +0530

    Update local_notifier.dart
    testing 2

--- lib/src/local_notifier.dart
+++ lib/src/local_notifier.dart
@@ -83,7 +83,7 @@ class LocalNotifier {
     ShortcutPolicy shortcutPolicy = ShortcutPolicy.requireCreate,
   }) async {
     final Map<String, dynamic> arguments = {
-      'display_name': appName,
+      'appName': appName,
       'shortcutPolicy': shortcutPolicy.name,
     };
     if (Platform.isWindows) {
@@ -105,7 +105,7 @@ class LocalNotifier {
     _notifications[notification.identifier] = notification;
 
     final Map<String, dynamic> arguments = notification.toJson();
-    arguments['display_name'] = _appName;
+    arguments['appName'] = _appName;
     await _channel.invokeMethod('notify', arguments);
   }
 
